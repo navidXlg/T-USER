7cf15ea7528be8517b51d734268de15e4ac94166
navidXlg <navidjoker669@gmail.com>
2024-08-27 16:58:41 +0330

    feat: add button atom

---
+++ src/components/atoms/Button/BaseButton/index.tsx
@@ -0,0 +1,15 @@
+// export interface IBaseButton {
+// 	label: string;
+// 	className: string;
+// 	disabled: boolean;
+// 	size: string;
+// 	type: string;
+// 	loading: boolean;
+// 	onClick: () => void;
+// }
+
+// export function BaseButton(props: IBaseButton) {
+// 	const { label, className, disabled } = props;
+
+// 	return <button></button>;
+// }
