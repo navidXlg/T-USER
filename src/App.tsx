808957c45dfb3d3c3ec63d082f3089e9b10d0f2f
navidXlg <navidjoker669@gmail.com>
2024-08-27 14:33:05 +0330

    feat: adding type aliasses to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import { DefaultLayout } from './layout/DeafultLayout';
 
-function App() {
+const routes = createBrowserRouter([
+	{
+		element: <DefaultLayout />,
+		children: [
+			{
+				path: '',
+				element: <LoginPage />,
+			},
+		],
+	},
+]);
+
+export function App() {
 	return <div>our new react applications!</div>;
 }
-
-export default App;
---
+++ src/components/pages/SubmitPage/index.tsx
@@ -0,0 +1,3 @@
+export function SubmitPage() {
+	return <main></main>;
+}
